0ebe711183d275e74a2e526edc82323e68d4c9f3
Dimitri Bonneville <dimitribonneville1@gmail.com>
2026-06-22 19:14:10 +0200

    Add requests and move sheet data to data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,15 @@
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.sheety_endpoint = os.environ["SHEETY_ENDPOINT"]
+        self.response = requests.get(
+            self.sheety_endpoint,
+            auth=(os.environ["SHEETY_USERNAME"], os.environ["SHEETY_PASSWORD"])
+        )
+
+    def get_data(self):
+        return self.response.json()
--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from pprint import pprint
+import requests_cache
+from requests_cache import DO_NOT_CACHE, NEVER_EXPIRE, CachedSession
+from datetime import datetime, timedelta
+
+requests_cache.install_cache()
+
+if __name__ == "__main__":
+    tomorrow = datetime.now() + timedelta(days=1)
+    six_months = datetime.now() + timedelta(days=180)
+
+    urls_expire_after = {
+        'https://api.sheety.co/': DO_NOT_CACHE,
+        '*': 3600,
+    }
+    session = CachedSession(urls_expire_after=urls_expire_after)
+
+    sheet_data = DataManager()
+    pprint(sheet_data.get_data())
